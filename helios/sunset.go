e0d27e94c3fc687aec982d0b389d846f1a53ef27
Paul Sexton <psexton@gnresound.com>
2014-09-05 12:35:50 -0500

    helios sunset syncs to s3

--- helios/sunset.go
+++ helios/sunset.go
@@ -18,6 +18,8 @@ along with Helios.  If not, see <http://www.gnu.org/licenses/>.
 package helios
 
 import (
+	"github.com/brettweavnet/gosync/gosync"
+	"github.com/mitchellh/goamz/aws"
 	"io/ioutil"
 	log "github.com/cihub/seelog"
 )
@@ -52,7 +54,26 @@ func Sunset(conf Config) (err error) {
 		}
 	}
 	
-	// @TODO step 4: sync the dir to s3
+	// step 4: sync the dir to s3
+	err = syncToS3(tempDir, conf)
+
 	return
 }
 
+func syncToS3(sourceDir string, conf Config) (err error) {
+	const concurrent = 20 // @MAGIC
+	auth, err := aws.GetAuth(conf.AWS.AccessKeyID, conf.AWS.SecretAccessKey)
+	if err != nil {
+		return
+	}
+	dest := "s3://" + conf.AWS.S3BucketName
+	log.Debug("auth:", auth)
+	log.Debug("source:", sourceDir)
+	log.Debug("dest:", dest)
+
+	syncPair := gosync.NewSyncPair(auth, sourceDir, dest)
+	syncPair.Concurrent = concurrent
+	err = syncPair.Sync()
+
+	return
+}
